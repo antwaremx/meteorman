d8ba2c1d85eee19874cc66af7f977f4ddcfe39d3
Iván Cabrera <hec_ivan_150@hotmail.com>
2020-10-23 01:00:37 -0500

    Fix: Copy/Paste in Mac OS installer has been fixed

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron';
 
 /**
  * Set `__static` path to static files in production
@@ -28,6 +28,26 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+  // Create the Application's main menu
+  const template = [{
+    label: "Application",
+    submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ]
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.on('ready', createWindow)
